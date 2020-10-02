020ce2349e993bf020b0b89283f33ec64ea38388
Aravind R <aravindr597@gmail.com>
2020-10-02 14:05:04 +0530

    fully functional

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var db = require('./config/connection')
 
 var app = express();
 
@@ -19,7 +19,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+db.connect((err)=>{
+  if(err) console.log("Connection Error"+err)
+  console.log("Database Connected Succesfully")
+})
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,80 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var JsAlert = require("js-alert");
+var mainfunction = require("../account_fun/main_fun");
+const { render } = require("../app");
 
 /* GET HOME page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Account Lookup Automator'});
-});
+router.get("/", (req, res, _next) => {
+  if (req.query.search) {//search
+    const regex = new RegExp(searchOptions(req.query.search), "gi");
 
+    mainfunction.searchAccount(regex).then((accounts) => {
+      res.render("index", { accounts: accounts });
+    });
+  } else {
+    mainfunction.findAccount().then((accounts) => {
+      res.render("index", { accounts: accounts });
+    });
+  }
+});
+//function for search
+function searchOptions(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
 
 /* GET ADD page. */
-router.get('/add', function(req, res,) {
-  res.render('add', { title: 'Account Lookup Automator'});
+router.get("/add", function (_req, res) {
+  res.render("add", { title: "Account Lookup" });
 });
 
 //Add Account
-router.post('/addAccount',function(req,res){
+router.post("/addAccount", function (req, res) {
   console.log(req.body);
-  res.send("Account Added to Database")
+
+  mainfunction.addAccount(req.body, () => {
+    res.redirect("/");
+  });
 });
 
-
 /* GET DELETE page. */
-router.get('/delete', function(req, res,) {
-  res.render('delete', { title: 'Account Lookup Automator'});
-
+router.get("/delete", function (_req, res) {
+  res.render("delete", { title: "Account Lookup" });
 });
 
 //Delete Account
-router.post('/deleteAccount',function(req,res){
+router.post("/deleteAccount", function (req, res) {
   console.log(req.body);
-  res.send("Account Deleted From Database")
+
+  mainfunction.deleteAccount(req.body, () => {
+    res.redirect("/");
+  });
+});
+//Get Final Page
+router.get("/final", function (_req, res) {
+  mainfunction.finalList().then((accounts) => {
+    res.render("final", { title: "Account Lookup", accounts: accounts });
+  });
+});
+//Add To List
+router.get("/addToList/:id", function (req, res) {
+  //console.log(req.params.id);
+  mainfunction.addToList(req.params.id, () => {
+    res.redirect("/");
+  });
+});
+//Delete Account From List
+router.get("/deleteFromList/:id", function (req, res) {
+  console.log(req.params.id);
+
+  mainfunction.deleteFromList(req.params.id, () => {
+    res.redirect("/final");
+  });
+});
+//Finish
+router.get("/finish", (req, res) => {
+  mainfunction.dropList();
+  res.redirect("/");
 });
 
 
